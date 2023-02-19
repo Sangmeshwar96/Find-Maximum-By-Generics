a68402a0bac58d7f90f567fbaf0f7d586f1ba319
Sangmeshwar Ramesh Patil <89923434+Sangmeshwar96@users.noreply.github.com>
2023-02-19 21:54:51 +0530

    [Sangmeshwar] Add: Finding Maximum Integer Value Among Three Values

---
+++ FindMaximum/FindMaximum/FindMax.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FindMaximum
+{
+    public class FindMax
+    {
+        public void ToCompare(int a, int b, int c)
+        {
+            if (a.CompareTo(b) > 0 && a.CompareTo(c) > 0)
+            {
+                Console.WriteLine(a+" is Maximum");
+            }
+            else if (b.CompareTo(a) > 0 && b.CompareTo(c) > 0)
+            {
+                Console.WriteLine(b+" is Maximum");
+            }
+            else
+            {
+                Console.WriteLine(c+" is Maximum");
+            }
+        }
+    }
+}
---
+++ FindMaximum/FindMaximum/Program.cs
@@ -0,0 +1,19 @@
+﻿using FindMaximum;
+using System;
+
+namespace FindMaximum
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Generics Program To Find The Maximum Values");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            FindMax check = new FindMax();
+            check.ToCompare(3, 2, 1);
+
+        }
+    }
+}
